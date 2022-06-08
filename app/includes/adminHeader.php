024ff171ba450b66801ed36ef88198f87f0293fd
Marko <marko.kovac.mk.2002@gmail.com>
2022-06-08 21:25:16 +0200

    uljepsan kod

--- app/includes/adminHeader.php
+++ app/includes/adminHeader.php
@@ -1,5 +1,5 @@
 <header class="navbar navbar-light sticky-top bg-light flex-md-nowrap p-0 shadow">
-  <a class="navbar-brand p-3" href="<?php echo BASE_URL . '/index.php'?>">
+  <a class="navbar-brand p-3" href="<?php echo BASE_URL . '/index.php' ?>">
     <img src="../../assets/images/logo.png" alt="" width="60" height="60" class="d-inline-block align-text-center">
     Udruga LePas
   </a>
