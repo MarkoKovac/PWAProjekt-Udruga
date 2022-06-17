a99bd3d53b73d38b93d5f1d48661400ba803a458
MarkoKovac <marko.kovac.mk.2002@gmail.com>
2022-06-17 16:18:04 +0200

    dodana ikona stranice

--- index.php
+++ index.php
@@ -16,6 +16,7 @@ error_reporting(0);
   <meta name="keywords" content="HTML, CSS, JavaScript">
   <meta name="author" content="Marko Kovac">
   <title>LePas Udruga za zaštitu životinja</title>
+  <link rel="shortcut icon" href="assets/images/favicon.ico"/>
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <!--Font awesome ikone-->
--- onama.php
+++ onama.php
@@ -9,6 +9,7 @@ error_reporting(0);
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>O nama - LePas Udruga za zaštitu životinja</title>
+  <link rel="shortcut icon" href="assets/images/favicon.ico"/>
   <!-- Bootstrap CSS -->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <!--Font awesome ikone-->
